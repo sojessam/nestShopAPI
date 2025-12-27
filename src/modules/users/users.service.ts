027f21ac11ea125551e1ceee05b50df964894f35
jessam <jessamtech@gmail.com>
2025-12-27 02:14:24 +0100

    feat: implement UserService to use hashed password

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import * as argon2 from 'argon2';
+import type { PrismaService } from 'src/common/prisma/prisma.service';
+import type { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { password, ...userData } = createUserDto;
+
+    const hashedPassword = await argon2.hash(password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          ...userData,
+          password: hashedPassword,
+        },
+      });
+
+      const { password: _, ...result } = user;
+
+      return result;
+    } catch (error) {
+      // @ts-expect-error: Prisma error code
+      if (error.code === 'P2002') {
+        throw new ConflictException('Email already in use.');
+      }
+      throw error;
+    }
+  }
+
+  async findOneByEmail(email: string) {
+    return this.prisma.user.findUnique({
+      where: { email },
+    });
+  }
+}
